d1019b0cf4e151a275f9b041da73965ed0ff8d7a
Thomas Perez <tomapego16@gmail.com>
2025-05-25 00:12:53 -0400

    push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: true,
   //apiUrl: 'http://localhost:3000/api' // Ajusta esta URL para que que funcione correctamente con Render
   apiUrl: 'https://jwhistoryback.onrender.com/api' // Ajusta esta URL para que que funcione correctamente con Render
-  apiUrl: 'https://jwhistoryback.onrender.com/api' // Ajusta esta URL para que que funcione correctamente con Render
 };
